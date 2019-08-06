4cf58154ae3cbc3dfe0b689b53b74a6a68e749b5
leonardoigor <igor_mendonca@outlook.com.br>
2019-08-06 14:25:22 -0300

    routes

--- app.js
+++ app.js
@@ -5,12 +5,37 @@ const RouterAdmin = require('./routes/Router')
 const handlebars = require('express-handlebars')
 const bodyparser = require('body-parser')
 const path = require('path')
+const mongoose = require('mongoose')
+const flash = require('connect-flash')
+const session = require('express-session')
 
 // varibles
 const PORT = 3000
 
+// flash msg session
+app.use(session({
+    secret: "car_gestao",
+    resave: true,
+    saveUninitialized: true
+}))
+app.use(flash())
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success')
+    res.locals.error = req.flash('error')
+    next()
+})
 // static files
 app.use(express.static(path.join(__dirname, 'src')))
+
+// mongoose
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/car_gestao', { useNewUrlParser: true })
+    .then(() => {
+        console.log('Connectado ao mongo')
+    })
+    .catch(e => {
+        console.log(`Erro ao se conectar: ${e}`)
+    })
 
 // body parse
 app.use(bodyparser.urlencoded({ extended: true }))
--- routes/Router.js
+++ routes/Router.js
@@ -1,10 +1,42 @@
 const router = require('express').Router()
+const mongoose = require('mongoose')
+// Load models files
+require('../models/User')
 
+// Models from mongoose
+const User = mongoose.model('users')
+// gettes
 router.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', { connected: false, name: '' })
+})
+router.get('/login', (req, res) => {
+    res.render('login_form')
+})
+router.get('/register', (req, res) => {
+    res.render('user_form')
 })
 router.get('/cars', (req, res) => {
     res.render('car_form')
+})
+
+// posts
+router.post('/register', (req, res) => {
+    const { username, name, password } = req.body
+    const newUser = {
+        username: username,
+        name: name,
+        password: password
+    }
+    new User(newUser).save()
+        .then(() => {
+            req.flash('success', 'Usuario cadastrado com sucesso')
+            res.redirect('/')
+        })
+        .catch(e => {
+            console.log(e)
+            req.flash('error', 'Houve algum erro do cadastrar usuario')
+            res.redirect('/')
+        })
 })
 
 module.exports = router
